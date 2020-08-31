108dc6082293a74e370e9784811f3e2aeae24cf7
Rodolfo Azevedo <rof20004@gmail.com>
2020-08-31 18:25:11 -0400

    Correção dos testes (#3)

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Maratona Full Cycle 4.0"', () => {
+      expect(appController.getHello()).toBe('Maratona Full Cycle 4.0');
     });
   });
 });
